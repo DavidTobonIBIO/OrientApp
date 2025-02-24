c5788eba96483001f08f89f5e62acfd60147e72f
DavidTobonIBIO <d.tobonm2@uniandes.edu.co>
2025-02-24 12:04:46 -0500

    idex buttons

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,20 +1,33 @@
+import icons from "@/constants/icons";
 import { Link } from "expo-router";
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, ScrollView, SafeAreaView, TouchableOpacity } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Image/>
-      <Text className="font-bold text-3xl my-10 font-spaceMono">Bienveido a OrientApp!</Text>
-      <Link href="/select-bus-route">SelectBusRoute</Link>
-      <Link href="/easy-guide">EasyGuide</Link>
-      <Link href="/user-guide">UserGuide</Link>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full justify-center items-center">
+        <Image source={icons.orientapp} className="w-5/6" resizeMode="contain" />
+        <View className="items-center w-5/6">
+          <Text className="font-bold text-3xl my-10 font-spaceMono text-center">
+            Bienvenido a OrientApp!
+          </Text>
+          <Link href="/select-bus-route" asChild>
+            <TouchableOpacity className="bg-dark-blue p-4 rounded-md my-2 w-5/6">
+              <Text className="text-white text-center">SelectBusRoute</Text>
+            </TouchableOpacity>
+          </Link>
+          <Link href="/easy-guide" asChild>
+            <TouchableOpacity className="bg-dark-blue p-4 rounded-md my-2 w-5/6">
+              <Text className="text-white text-center">EasyGuide</Text>
+            </TouchableOpacity>
+          </Link>
+          <Link href="/user-guide" asChild>
+            <TouchableOpacity className="bg-dark-blue p-4 rounded-md my-2 w-5/6">
+              <Text className="text-white text-center">UserGuide</Text>
+            </TouchableOpacity>
+          </Link>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
