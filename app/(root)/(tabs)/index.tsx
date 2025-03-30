a83014b2311c27c51c8cc4c3a01c82592d0b43f1
sergiocanar <s.canar@uniandes.edu.co>
2025-03-30 16:03:22 -0500

    add  voice-route.tsx and improve index.tsx

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,22 +1,44 @@
-import React from "react";
-import { Text, View, Image, ScrollView, SafeAreaView, TouchableOpacity, Platform } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  Image,
+  ScrollView,
+  SafeAreaView,
+  TouchableOpacity,
+  Platform,
+  Button,
+} from "react-native";
 import { Link, Href } from "expo-router";
+import { router } from "expo-router";
 import icons from "@/constants/icons";
+
 const API_BASE_URL = process.env.EXPO_PUBLIC_ORIENTAPP_API_BASE_URL;
 
-
 export default function Index() {
+  const [recognizedText, setRecognizedText] = useState('');
 
   const buttons: { label: string; link: Href }[] = [
+    { label: "Ruta por Voz", link: "/voice-route" as const },
     { label: "Seleccionar Ruta", link: "/select-bus-route" as const },
     { label: "Orienta Fácil", link: "/easy-guide" as const },
-    { label: "Guía de Usuario", link: "/user-guide" as const },
   ];
+
+  useEffect(() => {
+    const lowerText = recognizedText.toLowerCase();
+    if (lowerText.includes("seleccionar")) {
+      router.push("/select-bus-route");
+    } else if (lowerText.includes("orienta")) {
+      router.push("/easy-guide");
+    } else if (lowerText.includes("voz")) {
+      router.push("/voice-route");
+    }
+  }, [recognizedText]);
 
   return (
     <SafeAreaView className="bg-white h-full">
-      <ScrollView contentContainerClassName="h-full justify-center items-center">
-        <Image source={icons.orientapp} className="w-5/6" resizeMode="contain" />
+      <ScrollView className="w-full" contentContainerClassName="items-center justify-start pb-10">
+        <Image source={icons.orientapp} className="w-5/6 mt-10" resizeMode="contain" />
         <View className="items-center w-5/6">
           <Text className="font-bold text-5xl my-14 font-spaceMono text-center">
             Bienvenido a OrientApp!
@@ -33,6 +55,7 @@ export default function Index() {
               </TouchableOpacity>
             </Link>
           ))}
+
         </View>
       </ScrollView>
     </SafeAreaView>
